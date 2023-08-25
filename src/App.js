a975398d12c77391c3a10e99bd7fdfc3c0fa0672
Shafwan Fahim^C <fahims5@mcmaster.ca>
2023-08-25 17:42:54 -0400

    css updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react'; 
-import Navbar from './components/Navbar';  
-import Home from './pages/Home';  
-import{BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
- <div className='App'>
-  <Router>
-  <Navbar /> 
-  <Switch>
-    <Route path="/" exact Component={Home} />
-  </Switch>
-  </Router>
- </div>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
 export default App;
+

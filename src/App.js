cd8850b337b9633285c9df0a4488ac441f2a9214
Shafwan Fahim^C <fahims5@mcmaster.ca>
2023-08-24 23:21:59 -0400

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'; 
+import Navbar from './components/Navbar';  
+import Home from './pages/Home';  
+import{BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className='App'>
+  <Router>
+  <Navbar /> 
+  <Switch>
+    <Route path="/" exact Component={Home} />
+  </Switch>
+  </Router>
+ </div>
   );
 }
 

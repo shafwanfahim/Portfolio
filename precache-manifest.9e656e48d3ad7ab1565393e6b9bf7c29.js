d85135019f91556b275046ac95065c656c88e82b
Shafwan Fahim^C <fahims5@mcmaster.ca>
2023-09-09 00:02:25 -0400

    Updates

---
+++ precache-manifest.9e656e48d3ad7ab1565393e6b9bf7c29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10cdd812ffedf18bbcef",
+    "url": "/Portfolio/static/css/main.58b2f044.chunk.css"
+  },
+  {
+    "revision": "10cdd812ffedf18bbcef",
+    "url": "/Portfolio/static/js/main.10cdd812.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/Portfolio/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "52e222c874776836309e",
+    "url": "/Portfolio/static/js/2.52e222c8.chunk.js"
+  },
+  {
+    "revision": "2013c2bf0d7be5fc01e4",
+    "url": "/Portfolio/static/js/runtime~main.2013c2bf.js"
+  },
+  {
+    "revision": "0c49f6b20e0a5125304527247647324f",
+    "url": "/Portfolio/static/media/s.0c49f6b2.png"
+  },
+  {
+    "revision": "d71a164725ce80f7b3381823d9d39292",
+    "url": "/Portfolio/index.html"
+  }
+];
